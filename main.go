611a9a9afded576c7d10fc0ec034cf1e7977d893
theeddieh <theeddieharrison@gmail.com>
2019-02-28 23:18:52 -0800

    #1: Cleans up logging.

--- main.go
+++ main.go
@@ -13,19 +13,20 @@ func main() {
 	// this should accept a single argument on the command line
 	// pointing to the path of the input file
 	if len(os.Args) < 2 {
-		fmt.Println("No filepath specified")
+		fmt.Printf("No filepath specified\n")
+		fmt.Printf("Usage:\n")
+		fmt.Printf("  ./memdb <path to input file>\n")
 		return
 	}
 	infile, err := os.Open(os.Args[1])
 	if err != nil {
-		fmt.Printf("Failed to open file: %s", os.Args[1])
+		fmt.Printf("Failed to open file: %s\n", os.Args[1])
 		return
 	}
 	defer infile.Close()
 	fileScanner := bufio.NewScanner(infile)
 
 	d := db.New()
-
 	for fileScanner.Scan() {
 		if err != nil {
 			break
@@ -41,7 +42,7 @@ func main() {
 				d.Rollback()
 			case "#":
 			default:
-				fmt.Println(fmt.Errorf("unknown instruction `%s` found", command[0]))
+				fmt.Printf("unknown instruction `%s` found\n", command[0])
 				return
 			}
 		}
