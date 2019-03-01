d43e395bcbd2ad646de34ee721a486ec9aedd39c
theeddieh <theeddieharrison@gmail.com>
2019-02-28 22:34:15 -0800

    #1: Adds ROLLBACK command
    Rollback implementation details:
    * discards the notion of tracking command history that was previously skeletoned-out
    * instead, pushes the keys written or deleted on to a stack
    * the Database maps keys to []string, and uses that as a stack to push new values
    * deletes are handled internally as writing an empty string to a value

--- db/db.go
+++ db/db.go
@@ -4,14 +4,11 @@ import (
 	"fmt"
 )
 
-// Command is a single database command
-type command []string
-
 // Database is an in-memory KV store.
 // It tracks the current state and the transaction history.
 type Database struct {
-	state   map[string]string
-	history []command
+	state   map[string][]string
+	history []string
 }
 
 // DatabaseError handles errors specific to db operations.
@@ -27,37 +24,58 @@ const ErrMissingKey = DatabaseError("key not present")
 // New in-memory database.
 func New() (d *Database) {
 	return &Database{
-		state:   make(map[string]string),
-		history: make([]command, 0),
+		state:   make(map[string][]string),
+		history: make([]string, 1),
 	}
 }
 
 // Write the value `v` to the database under key `k`.
-func (d Database) Write(k, v string) {
-	d.state[k] = v
+func (d *Database) Write(k, v string) {
+	d.state[k] = append(d.state[k], v)
+	d.history = append(d.history, k)
 }
 
 // Read the value stored under key `k`. Returns an error message if the key is missing.
-func (d Database) Read(k string) (v string, err error) {
-	v, ok := d.state[k]
+func (d *Database) Read(k string) (v string, err error) {
+	vals, ok := d.state[k]
 	if !ok {
+		return "", ErrMissingKey
+	}
+
+	l := len(vals)
+	if l < 1 {
+		return "", ErrMissingKey
+	}
+
+	v = vals[len(vals)-1]
+	if v == "" {
 		err = ErrMissingKey
 	}
 	return v, err
 }
 
 // Delete the key `k` from the database.
-func (d Database) Delete(k string) {
-	delete(d.state, k)
+func (d *Database) Delete(k string) {
+	d.Write(k, "")
 }
 
 // Print the current state of the database.
-func (d Database) Print() {
+func (d *Database) Print() {
 	for k, v := range d.state {
-		fmt.Println(k, v)
+		latestValue := v[len(v)-1]
+		if latestValue != "" {
+			fmt.Println(k, latestValue)
+		}
 	}
 }
 
 // Rollback the database back to its state prior to the most recent command.
-func (d Database) Rollback() {
+// Pops most recently accessed key off of history stack, and then pops that latest value off of that key's stack.
+func (d *Database) Rollback() {
+	l := len(d.history) - 1
+	lastCommand := d.history[l]
+	d.history = d.history[:l]
+
+	l = len(d.state[lastCommand]) - 1
+	d.state[lastCommand] = d.state[lastCommand][:l]
 }
