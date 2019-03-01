ad52f4bcc5484a93b8469d3a95223a3194feef3e
theeddieh <theeddieharrison@gmail.com>
2019-02-28 23:21:24 -0800

    #1: Removes unreachable error check.

--- main.go
+++ main.go
@@ -28,23 +28,19 @@ func main() {
 
 	d := db.New()
 	for fileScanner.Scan() {
-		if err != nil {
-			break
-		} else {
-			switch command := strings.Fields(fileScanner.Text()); command[0] {
-			case "WRITE":
-				d.Write(command[1], command[2])
-			case "DELETE":
-				d.Delete(command[1])
-			case "PRINT":
-				d.Print()
-			case "ROLLBACK":
-				d.Rollback()
-			case "#":
-			default:
-				fmt.Printf("unknown instruction `%s` found\n", command[0])
-				return
-			}
+		switch command := strings.Fields(fileScanner.Text()); command[0] {
+		case "WRITE":
+			d.Write(command[1], command[2])
+		case "DELETE":
+			d.Delete(command[1])
+		case "PRINT":
+			d.Print()
+		case "ROLLBACK":
+			d.Rollback()
+		case "#":
+		default:
+			fmt.Printf("unknown instruction `%s` found\n", command[0])
+			return
 		}
 	}
 	return
